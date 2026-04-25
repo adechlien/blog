4ca519a3494986585c61df7978eaefe2f3f20cd3
adechlien <jhalejandro@uninorte.edu.com>
2026-04-25 17:56:32 -0500

    added vercel integration with Turso

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 import db from "@astrojs/db";
 
@@ -9,4 +10,5 @@ export default defineConfig({
   site: "https://adechlien.blog",
   output: "static",
   integrations: [mdx(), sitemap(), tailwind(), db()],
+  adapter: vercel(),
 });
