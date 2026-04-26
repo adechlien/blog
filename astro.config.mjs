47c1ce2ef3b7f768102f56c11d08628ebacb5035
adechlien <jhalejandro@uninorte.edu.com>
2026-04-25 20:21:36 -0500

    set output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
-
 import db from "@astrojs/db";
 
 export default defineConfig({
   site: "https://adechlien.blog",
+  output: "server",
   integrations: [mdx(), sitemap(), tailwind(), db()],
   adapter: vercel(),
 });
