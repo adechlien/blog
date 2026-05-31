6582272ff0cc62df824e7260aee5729d353244c2
adechlien <jhalejandro@uninorte.edu.co>
2026-05-31 11:29:42 -0500

    chore: update cms package.json and payload.config.ts

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -1,10 +1,11 @@
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
+
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { Collections } from './collections/Collections'
@@ -14,6 +15,9 @@ import { Figures } from './collections/Figures'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+const serverURL =
+  process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
 export default buildConfig({
   admin: {
     user: Users.slug,
@@ -21,25 +25,41 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
+
   collections: [Users, Media, Collections, Texts, Figures],
+
   editor: lexicalEditor(),
+
   secret: process.env.PAYLOAD_SECRET || '',
+
+  serverURL,
+
+  csrf: [
+    'http://localhost:3000',
+    'https://cms.adechlien.blog',
+    'https://adechlien.blog',
+  ],
+
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
+
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URL || '',
       authToken: process.env.TURSO_AUTH_TOKEN || '',
     },
   }),
+
   sharp,
+
   plugins: [
     vercelBlobStorage({
-        collections: {
-          media: true,
-        },
-        token: process.env.BLOB_READ_WRITE_TOKEN,
-      }),
+      enabled: Boolean(process.env.BLOB_READ_WRITE_TOKEN),
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
