cfc68ce8cc18456d2a8b00e1e72070b6b7ca7422
adechlien <jhalejandro@uninorte.edu.co>
2026-06-10 12:48:30 -0500

    fix: repair sketches migration

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260531_030918_init from './20260531_030918_init';
 import * as migration_20260607_162320_add_videos from './20260607_162320_add_videos';
 import * as migration_20260610_155138_add_sketches from './20260610_155138_add_sketches';
 import * as migration_20260610_161600_sketchbooks_model from './20260610_161600_sketchbooks_model';
+import * as migration_20260610_174500_repair_sketchbook_uploads from './20260610_174500_repair_sketchbook_uploads';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20260610_161600_sketchbooks_model.up,
     down: migration_20260610_161600_sketchbooks_model.down,
-    name: '20260610_161600_sketchbooks_model'
+    name: '20260610_161600_sketchbooks_model',
+  },
+  {
+    up: migration_20260610_174500_repair_sketchbook_uploads.up,
+    down: migration_20260610_174500_repair_sketchbook_uploads.down,
+    name: '20260610_174500_repair_sketchbook_uploads'
   },
 ];
